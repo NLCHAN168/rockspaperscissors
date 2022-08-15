5974cb9c57f5d42f61c2348cf793a0df7064db91
Nick <nlchan168@yahoo.com>
2022-08-15 00:25:32 -0400

    Fixed RPS game logic and renamed HTML file to 'Rocks, Paper, Scissors'.

--- index.js
+++ index.js
@@ -1,47 +1,72 @@
-const x = playerSelection;
-const y = getComputerChoice;
+let p = playerSelection();
+let c = getComputerChoice();
 
 function getComputerChoice() {
-    let x = Math.random()*100 + 1;
-    if (x < 33) {
-        console.log(x);
-        console.log("Computer chose Rock");
-        return "rock";
-    }
-    if (x > 33 && x < 66) {
-        console.log(x);
-        console.log("Computer chose Paper");
-        return "paper";
-    }
-    else {
-        console.log(x);
-        console.log("Computer chose Scissors");
-        return "scissors";
-    }
+  let playerSelection = Math.random() * 100 + 1;
+  if (playerSelection < 33) {
+    return "rock";
+  }
+  if (playerSelection > 33 && playerSelection < 66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
-function play(playerSelection, computerSelection) {
-    if (x === y) {
-        console.log("Player selected " + x);
-        console.log("Computer selected " + y);
-        console.log("Tie!");
-    }
-    else if (x == "rock" && y == "scissors") {
-        console.log("Player selected " + x);
-        console.log("Computer selected " + y);
-        console.log("Win!");
-    }
-    else if (x == "rock" && y == "paper") {
-        console.log("Player selected " + x);
-        console.log("Computer selected " + y);
-        console.log("Lose!");
-    }
+function play(playerSelection, getComputerChoice) {
+  p = playerSelection();
+  c = getComputerChoice();
+  if (p === c) {
+    console.log("Player selected " + p);
+    console.log("Computer selected " + c);
+    console.log("Tie!");
+    return "Tie!";
+  } else if (
+    (p == "rock" && c == "scissors") ||
+    (p == "paper" && c == "rock") ||
+    (p == "scissors" && c == "paper")
+  ) {
+    console.log("Player selected " + p);
+    console.log("Computer selected " + c);
+    console.log("Win!");
+    return "Win!";
+  } else if (
+    (p == "rock" && c == "paper") ||
+    (p == "paper" && c == "scissors") ||
+    (p == "scissors" && c == "rock")
+  ) {
+    console.log("Player selected " + p);
+    console.log("Computer selected " + c);
+    console.log("Lose!");
+    return "Lose!";
+  } else {
+    console.log("Invalid player choice.");
+  }
 }
 
 function playerSelection() {
-    let z = prompt("Please choose Rock, Paper, or Scissors");
-    z.toLowerCase();
-    return z;
+  let z = prompt("Please choose Rock, Paper, or Scissors");
+  return z.toLowerCase();
 }
 
-play(x, y);
+function game() {
+  let playerCounter = 0;
+  let computerCounter = 0;
+  let tie = 0;
+  for (let i = 0; i < 5; i++) {
+    let result = play(playerSelection, getComputerChoice);
+    if (result == "Win!") {
+      playerCounter++;
+    }
+    if (result == "Lose!") {
+      computerCounter++;
+    } else {
+      tie++;
+    }
+  }
+  console.log("Player wins: " + playerCounter);
+  console.log("Computer wins: " + computerCounter);
+  console.log("Ties: " + tie);
+}
+
+game();
